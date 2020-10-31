2ddb8dac41031090b729de78b4ae8b1b84e53a81
Surajv <vsuraj2405@gmail.com>
2020-10-31 21:00:37 +0530

    initial

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCRvAsdCsNhmYOlrS7MfgidqSKq-EJsm9k",
+    authDomain: "devmeetchat.firebaseapp.com",
+    databaseURL: "https://devmeetchat.firebaseio.com",
+    projectId: "devmeetchat",
+    storageBucket: "devmeetchat.appspot.com",
+    messagingSenderId: "258218487049",
+    appId: "1:258218487049:web:d5ff3d71ee5b3c0192b7d8",
+    measurementId: "G-QXBGNH4RN1"
+  };
