cb6bc827949f5b833158631b454e3a7234a4d2dc
Surajv <vsuraj2405@gmail.com>
2020-11-01 12:46:18 +0530

    firebase package added, authentication code.. provider google

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+
+import firebase from "firebase"; 
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCRvAsdCsNhmYOlrS7MfgidqSKq-EJsm9k",
@@ -9,3 +13,22 @@ const firebaseConfig = {
     appId: "1:258218487049:web:d5ff3d71ee5b3c0192b7d8",
     measurementId: "G-QXBGNH4RN1"
   };
+
+  // initialize app 
+
+  const firebaseApp = firebase.initializeApp
+  (firebaseConfig); 
+
+  // this would access our firestore instance 
+
+  const db = firebaseApp.firestore() ;
+
+  // authentication handler
+  const auth = firebase.auth() ; 
+
+  // google for authentication provider 
+  const provider = new firebase.auth.GoogleAuthProvider() ; 
+
+
+  export {auth , provider};
+  export default db; 
