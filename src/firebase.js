1188fb6044e71219052a3e2ea76264df9aca77c3
Suraj Verma <59371846+Surajv311@users.noreply.github.com>
2025-01-19 23:31:42 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAA1hiq5j4WkAAVsXu7W6EP1Jm2PQ0B3H8",
+  apiKey: "abcd1234",
   authDomain: "devs2chat.firebaseapp.com",
   databaseURL: "https://devs2chat.firebaseio.com",
   projectId: "devs2chat",
   storageBucket: "devs2chat.appspot.com",
-  messagingSenderId: "862913165344",
-  appId: "1:862913165344:web:f242b925314fafa1956445",
+  messagingSenderId: "abcd1234",
+  appId: "1:abcd1234:web:abcd1234",
   measurementId: "G-QQX414GRS7",
 };
 
